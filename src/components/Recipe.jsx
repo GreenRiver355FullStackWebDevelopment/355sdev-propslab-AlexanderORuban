63026b70ac4aad291e27aa55c59ee3353bff187c
Alexander Ruban <alexruban29@gmail.com>
2025-04-17 11:27:39 -0700

    Edited styling.

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,21 +4,21 @@ import RestaurantIcon from '@mui/icons-material/Restaurant';
 const Recipe = ({ recipe: { name, ingredients, instructions, optionalGlaze } }) => {
     return (
         <div>
-            <Typography variant='h1' sx={{color: 'crimson'}}>
+            <Typography variant='h1' sx={{color: 'burlywood'}}>
                 Recipes:
                 <RestaurantIcon sx={{ marginLeft: 1, fontSize: '70px' }} />
             </Typography> 
 
-            <Typography variant='h2' sx={{color: 'royalblue'}}>{name}</Typography>
+            <Typography variant='h2' sx={{color: 'beige'}}>{name}</Typography>
 
-            <Typography variant='h3' sx={{color: 'mediumseagreen'}}>Ingredients:</Typography>
+            <Typography variant='h3' sx={{color: 'gray'}}>Ingredients:</Typography>
             <ul>
                 {ingredients.map((item, index) => (
                     <li key={index} >{item}</li>
                 ))}
             </ul>
 
-            <Typography variant='h3' sx={{color: 'mediumseagreen'}}>Instructions:</Typography>
+            <Typography variant='h3' sx={{color: 'gray'}}>Instructions:</Typography>
             <ol>
                 {instructions.map((step, index) => (
                     <li key={index}>{step}</li>
@@ -27,16 +27,16 @@ const Recipe = ({ recipe: { name, ingredients, instructions, optionalGlaze } }) 
 
             {optionalGlaze && ( //JSX if statement on whether or not to render the optionalGlaze that some recipes include
                 <div className="optionalGlaze">
-                    <Typography variant='h4' sx={{color: 'sienna'}}>Optional Glaze: {optionalGlaze.name}</Typography>
+                    <Typography variant='h4' sx={{color: '#9E5B40'}}>Optional Glaze: {optionalGlaze.name}</Typography>
 
-                    <Typography variant='h4' sx={{color: 'slateblue'}}>Glaze Ingredients:</Typography>
+                    <Typography variant='h4' sx={{color: 'lightblue'}}>Glaze Ingredients:</Typography>
                     <ul>
                         {optionalGlaze.ingredients.map((item, index) => (
                             <li key={index}>{item}</li>
                         ))}
                     </ul>
 
-                    <Typography variant='h4' sx={{color: 'forestgreen'}}>Glaze Instructions:</Typography>
+                    <Typography variant='h4' sx={{color: 'lightgreen'}}>Glaze Instructions:</Typography>
                     <ol>
                         {optionalGlaze.instructions.map((step, index) => (
                             <li key={index}>{step}</li>
